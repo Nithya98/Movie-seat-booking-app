34a46b237b3e1b0b3ba5b9425e87db906b43c895
Nithya98 <nithyashanmugam03@gmail.com>
2021-07-03 00:56:36 +0530

    first commit

---
+++ Booking/script.js
@@ -0,0 +1,76 @@
+const container = document.querySelector('.container')
+const seats = document.querySelectorAll('.row .seat:not(.occupied)')
+
+const count = document.getElementById('count')
+const total = document.getElementById('total')
+const movieSelect = document.getElementById('movie')
+const bookNow = document.getElementById('booknow')
+
+// "+" is another way of converting the string to number instead of using parseInt
+let ticketPrice = +movieSelect.value
+
+// save selected movie index and price
+const setMovieData = (movieIndex, moviePrice) => {
+  localStorage.setItem('selectedMovieIndex', movieIndex)
+  localStorage.setItem('selectedMoviePrice', moviePrice)
+}
+
+// update total and count
+const updateSelectedCount = () => {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected')
+  const selectedSeatsCount = selectedSeats.length
+
+  const seatIndex = [...selectedSeats].map((seat) => {
+    return [...seats].indexOf(seat)
+  })
+
+  console.log(seatIndex)
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatIndex))
+
+  count.innerText = selectedSeatsCount
+  total.innerText = selectedSeatsCount * ticketPrice
+}
+
+// Get data from localStorage and populate UI
+const populateUI = () => {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'))
+  //   console.log(selectedSeats);
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected')
+      }
+    })
+  }
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex')
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex
+  }
+}
+
+populateUI()
+
+// Movie select ecent
+movieSelect.addEventListener('change', (e) => {
+  ticketPrice = e.target.value
+  setMovieData(e.target.selectedIndex, e.target.value)
+  updateSelectedCount()
+})
+
+container.addEventListener('click', (e) => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected')
+    updateSelectedCount()
+  }
+})
+
+bookNow.addEventListener('click', () => {
+  alert(`The selected movie has been booked successfully!!`)
+})
+
+// initial count and total seats
+updateSelectedCount()
